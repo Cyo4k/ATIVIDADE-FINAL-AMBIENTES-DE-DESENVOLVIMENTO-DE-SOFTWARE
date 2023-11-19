582b7b176f8f6843f4101d38605b7ff88f511377
gabrielbarboza01 <150488216+gabrielbarboza01@users.noreply.github.com>
2023-11-19 12:58:59 +0000

    Commit Gabriel

---
+++ src/Carro.java
@@ -0,0 +1,38 @@
+public class Carro {
+    public String marca;
+    public String modelo;
+    public int ano;
+    public int quilometragem;
+    public Motor motor;
+
+    public Carro() { }
+
+    public Carro(String chevrolet, String camaro, int i, Motor motorCarro) {
+    }
+
+    public void setCarroInfo(String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public int rodar(int quilometro) {
+        if (quilometro >= 0) {
+            quilometragem += quilometro;
+        }
+        return quilometragem;
+    }
+
+    public int getQuilometragem() {
+        return quilometragem;
+    }
+
+    @Override
+    public String toString() {
+        return "Marca: " + marca + "\nModelo: " + modelo + "\nAno: " + ano;
+    }
+
+    public void ligar() {
+        System.out.println("\nLigando o Carro...\n");
+    }
+}
